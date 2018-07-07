01e0c41dac4e9f6e5227fdbadc7975274026cdff
Leszek Mazur <valutcizen@gmail.com>
2018-07-07 23:03:37 +0200

    Zmiana struktury, dodana obsługa wejść/wyjść

--- SterowanieSpektrometr.ino
+++ SterowanieSpektrometr.ino
@@ -14,6 +14,15 @@
 #define serwo2KierunekPrawoPin 9
 #define serwo2EnkoderAPin 20
 #define serwo2EnkoderBPin 21
+//Konfiguracja uniwersalnych wejść / wyjść
+#define PinI1 10
+#define PinI2 11
+#define PinI3 12
+#define PinI4 13
+#define PinO1 14
+#define PinO2 15
+#define PinO3 16
+#define PinO4 17
 
 //PROGRAM
 Servo serwoLusterko;
@@ -31,9 +40,10 @@ Encoder enkoder2(serwo2EnkoderAPin, serwo2EnkoderBPin);
 
 struct DaneKomunikacja {
   volatile uint8_t Status = 0;
-  volatile uint8_t UstawionyKatLusterko = 0;
   volatile int16_t AktualneImpulsy1 = 0;
   volatile int16_t AktualneImpulsy2 = 0;
+  volatile uint8_t PortyIO = 0;
+  volatile uint8_t UstawionyKatLusterko = 0;
   volatile int16_t UstawioneImpulsy1 = 0;
   volatile int16_t UstawioneImpulsy2 = 0;
   volatile uint16_t PredkoscMax1 = 0;
@@ -56,10 +66,23 @@ void setup() {
   pinMode(serwo2EnkoderAPin, INPUT_PULLUP);
   pinMode(serwo2EnkoderBPin, INPUT_PULLUP);
 
+  pinMode(PinI1, INPUT_PULLUP);
+  pinMode(PinI2, INPUT_PULLUP);
+  pinMode(PinI3, INPUT_PULLUP);
+  pinMode(PinI4, INPUT_PULLUP);
+  pinMode(PinO1, OUTPUT);
+  pinMode(PinO2, OUTPUT);
+  pinMode(PinO3, OUTPUT);
+  pinMode(PinO4, OUTPUT);
+
   digitalWrite(serwo1EnkoderAPin, HIGH);
   digitalWrite(serwo1EnkoderBPin, HIGH);
   digitalWrite(serwo2EnkoderAPin, HIGH);
   digitalWrite(serwo2EnkoderBPin, HIGH);
+  digitalWrite(PinI1, HIGH);
+  digitalWrite(PinI2, HIGH);
+  digitalWrite(PinI3, HIGH);
+  digitalWrite(PinI4, HIGH);
 
   Serial.begin(115200);
   serwoLusterko.attach(serwoLusterkoPin, 1, 2);
@@ -77,6 +100,8 @@ void loop() {
   Dane.AktualneImpulsy1 = enkoder1.read();
   Dane.AktualneImpulsy2 = enkoder2.read();
 
+  ObsluzIO();
+
   //Dyskretny kontroler PI: https://www.embeddedrelated.com/showarticle/121.php
   
   delay(20);
@@ -86,3 +111,22 @@ void serialEvent(){
   while (Serial.available())
     DaneProtokol.GetedByte((uint8_t)Serial.read());
 }
+
+
+#define ReadPin(value, pin, b) if (digitalRead((pin))) bitSet((value), (b)); else bitClear((value), (b));
+#define WritePin(value, pin, b) if (bitRead((value),(b))) digitalWrite((pin), HIGH); else digitalWrite((pin), LOW);
+void ObsluzIO()
+{
+  uint8_t x = Dane.PortyIO;
+  ReadPin(x,PinI1,0);
+  ReadPin(x,PinI2,1);
+  ReadPin(x,PinI3,2);
+  ReadPin(x,PinI4,3);
+  WritePin(x,PinO1,4);
+  WritePin(x,PinO2,5);
+  WritePin(x,PinO3,6);
+  WritePin(x,PinO4,7);
+
+  Dane.PortyIO = x;
+}
+
